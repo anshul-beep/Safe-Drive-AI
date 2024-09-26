82150840e5ad15b3768e6e60505bfb4c50867c59
John Doe <21mm01023@iitbbs.ac.in>
2024-09-26 20:58:53 +0530

    static directory

--- drowsiness_detection_project/drowsiness_detection_project/settings.py
+++ drowsiness_detection_project/drowsiness_detection_project/settings.py
@@ -149,15 +149,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_BASE_DIR=BASE_DIR/'staticfiles'
-STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
-STATICFILES_VENDOR_DIR=STATICFILES_BASE_DIR/"vendors"
+import os
 
-# source for python manage.py collectstatics
-STATICFILES_DIRS=[
-    STATICFILES_BASE_DIR
-]
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
