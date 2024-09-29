167808c600f47d7a55a19fda08bc3df5116fa5ff
John Doe <21mm01023@iitbbs.ac.in>
2024-09-29 13:38:42 +0530

    changes setting

--- drowsiness_detection_project/drowsiness_detection_project/asgi.py
+++ drowsiness_detection_project/drowsiness_detection_project/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from drowsiness_detection_project.detection_app.consumers import DrowsinessConsumer  # Adjust this import as necessary
+from detection_app.consumers import DrowsinessConsumer  # Adjust this import as necessary
 from django.urls import path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drowsiness_detection_project.settings')
